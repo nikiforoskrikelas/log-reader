5da45f6474b430877638872cb43ea0d1262478d4
Nikiforos Krikelas <nikiforos_98@hotmail.gr>
2022-04-29 10:11:51 +0100

    Initial Commit

---
+++ src/main/java/ServerLogReader.java
@@ -0,0 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class ServerLogReader {
+    private static final Logger LOGGER = LogManager.getLogger(ServerLogReader.class);
+
+
+    public static void main(String[] args) {
+        LOGGER.info("Running Log Reader");
+
+
+    }
+}
